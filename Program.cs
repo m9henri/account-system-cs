c54f7121bf10f2484b651ede6860041434cd6261
m9henri <missolhenri@gmail.com>
2026-08-27 14:09:40 +0200

    actual initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace account_system_cs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
